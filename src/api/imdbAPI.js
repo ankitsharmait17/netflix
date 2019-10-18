2d814b1533503ca45695a70fbee6c4805ad1341e
Ankit Sharma <asharma@pipingrock.com>
2019-10-18 17:15:07 +0530

    api changed to https

--- src/api/imdbAPI.js
+++ src/api/imdbAPI.js
@@ -2,7 +2,7 @@ import { handleResponse, handleError } from "./apiUtils";
 
 export function getImdbRating(id) {
   const apiKey = "1181fcb0";
-  const url = "http://www.omdbapi.com";
+  const url = "https://www.omdbapi.com";
   return fetch(`${url}/?apiKey=${apiKey}&i=${id}`)
     .then(handleResponse)
     .catch(handleError);
