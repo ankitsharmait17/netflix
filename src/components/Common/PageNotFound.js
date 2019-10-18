b14ed51113c1c42945c79087cde297031c5226f3
ankit.sharma.it2017@gmail.com <ankit.sharma.it2017@gmail.com>
2019-10-18 11:03:04 +0530

    home button added to page not found

--- src/components/Common/PageNotFound.js
+++ src/components/Common/PageNotFound.js
@@ -1,5 +1,23 @@
 import React from "react";
+import { Container, Row, Col } from "reactstrap";
+import { Link } from "react-router-dom";
 
-const PageNotFound = () => <h1>Oops! Page not found.</h1>;
+const PageNotFound = () => {
+  return (
+    <Container style={{ color: "white", marginTop: "20px" }}>
+      <Row>
+        <Link to="/" className="btn btn-secondary">
+          Home
+        </Link>
+      </Row>
+      <br />
+      <Row>
+        <Col lg="12" className="text-center">
+          <h1>Oops!!Page not found.</h1>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
 export default PageNotFound;
